cde0396764bf9eef118e7ca9331314847ecc848b
Dima <galomko.dmytro@gmail.com>
2018-08-04 18:23:01 +0300

    add connection.properties

--- src/main/java/dao/Connection/ConnectionPoolMySql.java
+++ src/main/java/dao/Connection/ConnectionPoolMySql.java
@@ -1,6 +1,7 @@
 package dao.Connection;
 
-
+import dao.interfaces.ConnectionPoolDao;
+import exceptions.DBException;
 import org.apache.commons.dbcp.ConnectionFactory;
 import org.apache.commons.dbcp.DriverManagerConnectionFactory;
 import org.apache.commons.dbcp.PoolableConnectionFactory;
@@ -9,96 +10,64 @@ import org.apache.commons.pool.impl.GenericObjectPool;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.util.ResourceBundle;
 
-public class ConnectionPoolMySql {
+public class ConnectionPoolMySql implements ConnectionPoolDao {
 
-    static final String DB_URL = "jdbc:mysql://localhost:3306/bookstore?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&autoReconnect=true&useSSL=false"; //----------
-    public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static String URL = "jdbc:mysql://localhost:3306/bookstore?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&autoReconnect=true&useSSL=false";
+    private static final ResourceBundle QUERIES = ResourceBundle.getBundle("DBConnection");
+    //    private static String URL = "jdbc:mysql://localhost:3306/bookstore?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&autoReconnect=true&useSSL=false";
     private GenericObjectPool connectionPool = null;
-    public static final String USERNAME = "root";
-    public static final String PASSWORD = "1123581321";
 
     private static ConnectionPoolMySql instance = null;
 
-    private ConnectionPoolMySql(){}
+    private ConnectionPoolMySql() {
+    }
 
     public static ConnectionPoolMySql getInstance() {
-        if(instance == null) {
+        if (instance == null) {
             instance = new ConnectionPoolMySql();
         }
-
         return instance;
     }
 
+    public Connection getConnection() throws DBException {
+        try {
+            DataSource dataSource = setUp();
+            return dataSource.getConnection();
+        } catch (Exception exception) {
+            throw new DBException(exception);
+        }
+    }
+
     private DataSource setUp() throws Exception {
-        Class.forName(DRIVER).newInstance();
+        Class.forName(QUERIES.getString("mysql.driver")).newInstance();
 
-        // Creates an instance of GenericObjectPool that holds our
-        // pool of connections object.
         connectionPool = new GenericObjectPool();
-        connectionPool.setMaxActive(10);
-        connectionPool.setMaxIdle(5);
-        connectionPool.setMinIdle(3);
+        connectionPool.setMaxActive(Integer.valueOf(QUERIES.getString("mysql.maxActive")));
+        connectionPool.setMaxIdle(Integer.valueOf(QUERIES.getString("mysql.maxIdle")));
+        connectionPool.setMinIdle(Integer.valueOf(QUERIES.getString("mysql.minIdle")));
 
-        // Creates a connection factory object which will be use by
-        // the pool to create the connection object. We passes the
-        // JDBC url info, username and password.
         ConnectionFactory cf = new DriverManagerConnectionFactory(
-                URL,
-                USERNAME,
-                PASSWORD);
+                prepareURL(),
+                QUERIES.getString("mysql.username"),
+                QUERIES.getString("mysql.password"));
 
-        // Creates a PoolableConnectionFactory that will wraps the
-        // connection object created by the ConnectionFactory to add
-        // object pooling functionality.
         PoolableConnectionFactory pcf =
                 new PoolableConnectionFactory(cf, connectionPool,
                         null, null, false, true);
         return new PoolingDataSource(connectionPool);
     }
 
-    public Connection getConnection() throws Exception {
-        DataSource dataSource = setUp();
-        return dataSource.getConnection();
+    private String prepareURL() {
+        StringBuilder buildUrl = new StringBuilder();
+        buildUrl.append(QUERIES.getString("mysql.url")).append("&");
+        buildUrl.append(QUERIES.getString("mysql.useJDBCCompliantTimezoneShift")).append("&");
+        buildUrl.append(QUERIES.getString("mysql.useLegacyDatetimeCode")).append("&");
+        buildUrl.append(QUERIES.getString("mysql.serverTimeZone")).append("&");
+        buildUrl.append(QUERIES.getString("mysql.autoReconnect")).append("&");
+        buildUrl.append(QUERIES.getString("mysql.useSSL"));
+        return buildUrl.toString();
     }
-
-//    private ConnectionPoolMySql() {
-//
-//    }
-//
-//    public static ConnectionPoolMySql getInstance() {
-//        if (instance == null) {
-//            instance = new ConnectionPoolMySql();
-//        }
-//
-//        return instance;
-//    }
-//
-//    private DataSource setUp() throws Exception {
-//        Class.forName(DRIVER);
-//        GenericObjectPool genericObjectPool = new GenericObjectPool();
-//        genericObjectPool.setMaxActive(MAX_TOTAL);
-//        genericObjectPool.setMaxIdle(MAX_IDLE);
-//
-//        ConnectionFactory conFactory = new DriverManagerConnectionFactory(URI, USERNAME, PASSWORD);
-//        PoolableConnectionFactory poolableConnectionFactory =
-//                new PoolableConnectionFactory(conFactory, genericObjectPool,
-//                        null, null, false, true);
-//        return new PoolingDataSource(genericObjectPool);
-//    }
-//
-//    public Connection getConnection() {
-//        try {
-//            DataSource dataSource = setUp();
-//            Connection connection = dataSource.getConnection();
-//            return connection;
-//        } catch (Exception ex) {
-//            return null;
-//        }
-//    }
-
-
 }
 
 
--- src/main/java/dao/interfaces/ConnectionPoolDao.java
+++ src/main/java/dao/interfaces/ConnectionPoolDao.java
@@ -4,6 +4,6 @@ import exceptions.DBException;
 
 import java.sql.Connection;
 
-public interface ConnectionPool {
+public interface ConnectionPoolDao {
     Connection getConnection() throws DBException;
 }
